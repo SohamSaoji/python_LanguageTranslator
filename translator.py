cb29af7d81da3655a31845ef60623d246b2876bc
Soham Saoji <programmer.soham@gmail.com>
2020-06-28 13:03:15 +0530

    translator

---
+++ translator.py
@@ -0,0 +1,6 @@
+from translate import Translator
+
+translator = translator(from_lang='LANGUAGE', to_lang= 'LANGUAGE')
+translation - translator.translatr('SENTENCE YOU WANT TO TRANSLATE')
+
+print(translation)
